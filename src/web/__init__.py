6d0dda14ff9fdde071f32402acc7daefcf44d39f
abcdw <andrewtropin@gmail.com>
2015-10-26 18:21:58 +0300

    Create articles api url

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask import render_template
+from flask import request
+from database import search_by_title
+
 # import some db stuff
 import json
 
@@ -9,16 +12,33 @@ app = Flask(__name__)
 @app.route("/")
 def hello(name=None):
     # render static
-    return render_template('index.html', name=name)
+    return render_template('table.html', name=name)
     return "Hello World!"
 
 
 @app.route("/articles")
-@app.route("/articles/<range>")
+@app.route("/articles/<q>")
 def get_articles(range=None):
     # return json response
-    articles = range(0, 10)
-    return json.dumps(articles)
+    #  articles = range(0, 10)
+    #  print 'ok'
+    q = 'test'
+    query = request.args.get('q', 'test')
+    rows = search_by_title(query, 'stonedb', 'postgres', 'postgres')
+    result = []
+    for row in rows:
+        rrow = {'id': row[0],
+                'title': row[1],
+                'year': row[2],
+                'venue_id': row[3]
+                }
+        result.append(rrow)
+    #  print 'ok'
+    articles = [
+            { 'id': 0,
+              'name': 'Andrew'}
+            ]
+    return json.dumps(result)
 
 
 @app.route("/article")
--- src/web/database.py
+++ src/web/database.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" 
+"""
 Created on Tue Sep 22 16:26:39 2015
 on python3
 @author: Konstantin Sozykin
@@ -20,50 +20,50 @@ def do_query(query,dbname,user,passw,server = 'localhost'):
         print("Can't execute")
     result_rows = cur.fetchall()
     conn.close()
-    return result_rows 
+    return result_rows
 
 def search_by_year(year,dbname,user,passw,server = 'localhost'):
     q = "SELECT * FROM paper WHERE year =" + year
     return do_query(q,dbname,user,passw)
-    
-def search_by_title(template,dbname,user,passw,server = 'localhost'):
-    q = "SELECT * FROM paper WHERE title LIKE '%"+ template +"%'"
+
+def search_by_title(pattern,dbname,user,passw,server = 'localhost'):
+    q = "SELECT * FROM paper WHERE title LIKE '%"+ pattern +"%'"
     return do_query(q,dbname,user,passw)
 
-def search_by_author(template,dbname,user,passw,server = 'localhost'):
-    q = "SELECT * FROM paper AS p, author AS a WHERE p.pid = a.aid AND a.name = '" + template + "'"
+def search_by_author(pattern,dbname,user,passw,server = 'localhost'):
+    q = "SELECT * FROM paper AS p, author AS a WHERE p.pid = a.aid AND a.name = '" + pattern + "'"
     return do_query(q,dbname,user,passw)
-    
-def search_by_venue(template,dbname,user,passw,server = 'localhost'):
-    q = "SELECT * FROM paper AS p, venue as v WHERE p.venue_id = v.vid AND v.name LIKE '%"+ template +"%'"
+
+def search_by_venue(pattern,dbname,user,passw,server = 'localhost'):
+    q = "SELECT * FROM paper AS p, venue as v WHERE p.venue_id = v.vid AND v.name LIKE '%"+ pattern +"%'"
     return do_query(q,dbname,user,passw)
-    
-def search_by_keyword(template,dbname,user,passw,server = 'localhost'):
-    q = "SELECT p.* FROM paper AS p, keyword AS k, contains AS c WHERE p.pid = c.paper_id AND k.kid = c.keyword_id AND k.value = LIKE '%"+ template +"%'"
+
+def search_by_keyword(pattern,dbname,user,passw,server = 'localhost'):
+    q = "SELECT p.* FROM paper AS p, keyword AS k, contains AS c WHERE p.pid = c.paper_id AND k.kid = c.keyword_id AND k.value = LIKE '%"+ pattern +"%'"
     return do_query(q,dbname,user,passw)
-    
-def search_related_article(template,dbname,user,passw,server = 'localhost'):
-    q = "SELECT * FROM paper AS p WHERE pid IN ( SELECT to_id FROM refs WHERE from_id = " + template + ")"
+
+def search_related_article(pattern,dbname,user,passw,server = 'localhost'):
+    q = "SELECT * FROM paper AS p WHERE pid IN ( SELECT to_id FROM refs WHERE from_id = " + pattern + ")"
     return do_query(q,dbname,user,passw)
-    
+
 def search_related_article2(dbname,user,passw,server = 'localhost'):
     q = "SELECT p.* FROM paper as p, refs as r WHERE r.to_id = p.pid"
-    return do_query(q,dbname,user,passw) 
-    
+    return do_query(q,dbname,user,passw)
+
 def sort_by_popularity(dbname,user,passw,server = 'localhost'):
     q = "SELECT p.pid, COUNT(r.to_id) FROM paper AS p, refs AS r WHERE p.pid = r.to_id GROUP BY p.pid ORDER BY COUNT(r.to_id) DESC"
-    return do_query(q, dbname,user,passw) 
-    
+    return do_query(q, dbname,user,passw)
+
 def sort_by_count_refs(dbname,user,passw,server = 'localhost'):
     q = "SELECT p.pid, COUNT(r.from_id) FROM paper AS p, refs AS r WHERE p.pid = r.from_id GROUP BY p.pid ORDER BY COUNT(r.from_id) DESC"
-    return do_query(q, dbname,user,passw) 
-    
+    return do_query(q, dbname,user,passw)
+
 def sort_by_year(dbname,user,passw,server = 'localhost'):
     q = "SELECT * from paper ORDER BY year"
     return do_query(q, dbname,user,passw)
-    
+
 def insert_values(table_name,values,dbname,user,passw,server = 'localhost'):
-    query = "INSERT INTO " + table_name + " VALUES " + values 
+    query = "INSERT INTO " + table_name + " VALUES " + values
     try:
         conn = psycopg2.connect("dbname=" + dbname + " user=" + user +" host= " + server  + " password=" + passw)
     except:
@@ -75,9 +75,9 @@ def insert_values(table_name,values,dbname,user,passw,server = 'localhost'):
         print("Can't execute")
     conn.commit()
     conn.close()
-    
+
 def delete_paper_by_id(pid, dbname,user,passw,server = 'localhost'):
-    query = "DELETE FROM paper WHERE pid = " + pid 
+    query = "DELETE FROM paper WHERE pid = " + pid
     try:
         conn = psycopg2.connect("dbname=" + dbname + " user=" + user +" host= " + server  + " password=" + passw)
     except:
@@ -90,4 +90,4 @@ def delete_paper_by_id(pid, dbname,user,passw,server = 'localhost'):
         print("Can't execute")
     conn.commit()
     conn.close()
-    
+
