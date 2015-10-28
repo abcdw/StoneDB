b8326a7bc96a64b5a791cafe3d1eec4f44cb5c19
abcdw <andrewtropin@gmail.com>
2015-10-28 18:50:12 +0300

    Add http authentication and create_article()

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request, Response
+from functools import wraps
 from database import search_by_title
 
 # import some db stuff
 import json
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello(name=None):
@@ -30,18 +32,50 @@ def get_articles(range=None):
         rrow = {'id': row[0],
                 'title': row[1],
                 'year': row[2],
-                'venue_id': row[3]
+                # 'venue_id': row[3]
                 }
         result.append(rrow)
-    #  print 'ok'
-    articles = [
-            { 'id': 0,
-              'name': 'Andrew'}
-            ]
     return json.dumps(result)
 
 
+@app.route("/create_article", methods=['GEt', 'POST'])
+def create_article():
+    if request.method == 'POST':
+        print request.form
+        # get some values from request.form
+        return 'Render paper added page'
+        # do some magic stuff
+    if request.method == 'GET':
+        return 'Render form here'
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'admin'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
 @app.route("/article")
+@requires_auth
 def get_article():
     return "Article"
 
